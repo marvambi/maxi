4bfecb349926b4d034370988e19fe3736403e2ec
Marvin Ambrose <marvambi@primal.com.ng>
2020-06-28 19:56:10 +0100

    Added the Catalogue dashboard update III

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,6 @@ import 'package:marvin/screens/WelcomeScreen.dart';
 import 'package:marvin/screens/ImageSplashScreen.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-
 // Future main() async {
 //   runApp(new MaterialApp(
 //     title: 'Marvin',
@@ -34,22 +33,22 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Marvin',
-        theme: ThemeData(
-          // We set Poppins as our default font
-          textTheme: GoogleFonts.poppinsTextTheme(Theme.of(context).textTheme),
-          primaryColor: kPrimaryColor,
-          accentColor: kPrimaryColor,
-          visualDensity: VisualDensity.adaptivePlatformDensity,
-        ),
-        home: new AnimatedSplashScreen(),
-        routes: <String, WidgetBuilder>{
-          WELCOME_SCREEN: (BuildContext context) => WelcomeScreen(),
-          PRODUCT_SCREEN: (BuildContext context) => ProductsScreen(),
-          IMAGE_SPLASH: (BuildContext context) => ImageSplashScreen(),
-          ANIMATED_SPALSH: (BuildContext context) => AnimatedSplashScreen()
-        },
-        
+      debugShowCheckedModeBanner: false,
+      title: 'Marvin',
+      theme: ThemeData(
+        // We set Poppins as our default font
+        textTheme: GoogleFonts.poppinsTextTheme(Theme.of(context).textTheme),
+        primaryColor: kPrimaryColor,
+        accentColor: kPrimaryColor,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: new AnimatedSplashScreen(),
+      routes: <String, WidgetBuilder>{
+        WELCOME_SCREEN: (BuildContext context) => WelcomeScreen(),
+        PRODUCT_SCREEN: (BuildContext context) => ProductsScreen(),
+        IMAGE_SPLASH: (BuildContext context) => ImageSplashScreen(),
+        ANIMATED_SPALSH: (BuildContext context) => AnimatedSplashScreen()
+      },
+    );
   }
 }
