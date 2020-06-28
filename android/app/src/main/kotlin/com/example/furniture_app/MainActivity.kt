75e71e0649e9033ea52b5412be8eca153908ed42
Marvin Ambrose <marvambi@primal.com.ng>
2020-06-28 02:12:35 +0100

    Initialised afresh

---
+++ android/app/src/main/kotlin/com/example/furniture_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.marvambi.marvin
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:marvin/constants.dart';
+import 'package:marvin/screens/product/products_screen.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Marvin',
+      theme: ThemeData(
+        // We set Poppins as our default font
+        textTheme: GoogleFonts.poppinsTextTheme(Theme.of(context).textTheme),
+        primaryColor: kPrimaryColor,
+        accentColor: kPrimaryColor,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: ProductsScreen(),
+    );
+  }
+}
